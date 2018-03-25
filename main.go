d1d729c1fe8e956f03e5d2e07bb4087ab9e08753
Umesh Nadig <umesh@Umeshs-MacBook-Pro.local>
2018-03-25 14:33:28 +0530

    added few more methods

--- main.go
+++ main.go
@@ -1,12 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
 
-func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8080", nil)
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+type weatherData struct {
+	Name string 'json:"name"'
+	Main struct {
+		Kelvin float64 'json:"temp"'
+	} 'json:"main"'
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello"))
+func query(city strig) (weatherData, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=175ca570b998ca4c96000131784bf3fd&q=" + city)
+	if(err != nil) {
+		return weatherData{}, err
+	}
+	
+	defer resp.Body.Close()
+	var d weatherData
+	
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+	
+	return d, nil
+}
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.Run(":" + port)
 }
