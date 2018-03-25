14d7e35483e08b4f7ded350074b28cae9c45d22b
Umesh Nadig <umesh@Umeshs-MacBook-Pro.local>
2018-03-25 14:46:33 +0530

    added few more methods

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -10,25 +11,25 @@ import (
 )
 
 type weatherData struct {
-	Name string 'json:"name"'
+	Name string `json:"name"`
 	Main struct {
-		Kelvin float64 'json:"temp"'
-	} 'json:"main"'
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
 }
 
-func query(city strig) (weatherData, error) {
+func query(city string) (weatherData, error) {
 	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=175ca570b998ca4c96000131784bf3fd&q=" + city)
-	if(err != nil) {
+	if err != nil {
 		return weatherData{}, err
 	}
-	
+
 	defer resp.Body.Close()
 	var d weatherData
-	
+
 	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
 		return weatherData{}, err
 	}
-	
+
 	return d, nil
 }
 
